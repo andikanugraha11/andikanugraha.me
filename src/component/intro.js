17a729ac3c25a17d75155963c887bb2d37a1ed42
AndikaNugraha11 <m.andika.nugraha@gmail.com>
2017-12-04 16:56:47 +0700

    Update intro

--- src/component/intro.js
+++ src/component/intro.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import './test.css';
+import './home.css';
 
 class Intro extends Component {
 
@@ -9,18 +9,25 @@ class Intro extends Component {
             <section className="intro">
                 <div className="sp"></div>
                 <div className="intro-content">
-                    <img src="http://propertheme.com/img/slider/my-photo.jpg" alt=""/>
+                    <img src="https://instagram.fcgk10-1.fna.fbcdn.net/t51.2885-15/e35/16122725_164386350720746_4895664097783185408_n.jpg" alt=""/>
                     <div class="intro-text">
                         <h1>Hello I'am <strong> Andika Nugraha</strong></h1>
                         <div class="cd-headline clip is-full-width">
-                            <span>I am web</span>
+                            <span>I am </span>
                             <span class="cd-words-wrapper">
-                                <b class="is-visible">developer</b>
-                                <b>designer</b>
-                                <b>blogger</b>
+                                <b class="is-visible">web developer</b>
+                                <b>android developer</b>
+                                <b>fullstack developer</b>
                             </span>
                         </div>		
-                        <p>Founder of ProperTheme. Professonal ui/ux and frontend developer based on germany. Aviable of freelancer.</p>
+                        <p>based on Jabodetabek</p>
+                    </div>
+                    <div class="social-links">
+                        <a href="/"><i className="fa fa-github"></i></a>
+                        <a href="/"><i className="fa fa-google"></i></a>
+                        <a href="/"><i className="fa fa-linkedin"></i></a>
+                        <a href="/"><i className="fa fa-instagram"></i></a>
+                        <a href="/"><i className="fa fa-facebook"></i></a>
                     </div>
                 </div>
             </section>
